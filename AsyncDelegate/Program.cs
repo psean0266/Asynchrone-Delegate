50ba29d09157d5e743d10d789ff52b093a20a1a1
AbdoulayePangnonKOUL <AbdoulayePangnonKOUL@TAL-7G6S793>
2023-12-02 07:13:15 +0100

    commit init

---
+++ AsyncDelegate/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+
+namespace AsyncDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var webclient = new WebClient();
+
+            Console.Write("Téléchargement...");
+            string url = "https://codeavecjonathan.com/res/bateau.jpg";
+            webclient.DownloadFile(url,"bateau.jpg");
+            Console.WriteLine("OK");
+            
+        }
+    }
+}
